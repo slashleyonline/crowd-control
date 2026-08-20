f596eca92e3b6514c6c3c928f408b851702a98a4
Ian Keilman <iankeilman@proton.me>
2026-08-20 00:28:20 -0700

    Refactored FSM to use state objects

--- cc-project/crowdAgent.gd
+++ cc-project/crowdAgent.gd
@@ -1,7 +1,33 @@
 extends CharacterBody3D
 
-enum States {IDLE, WALKING, FROZEN, FLEEING, RETURNING}
-var state = States.IDLE
+#with this, each state can handle its own update logic
+class IdleState:
+	func update(agent):
+		var new_velocity= Vector3.ZERO
+
+		agent.randomize_target_location()
+
+		if !agent.nav_agent.is_navigation_finished():
+			agent.state  = agent.walking_state
+
+		return new_velocity  
+
+class WalkingState:
+	func update(agent):
+		var current_location = agent.global_transform.origin
+		var next_location = agent.nav_agent.get_next_path_position()
+		var new_velocity = (next_location  - current_location).normalized() * 10
+
+		if agent.nav_agent.is_navigation_finished():
+			agent.state = agent.idle_state
+			new_velocity = Vector3.ZERO
+
+		return new_velocity
+
+#starts in the idle state
+var idle_state = IdleState.new()
+var walking_state = WalkingState.new()
+var state=idle_state
 
 #higher than usual for testing purposes. should be 3.
 const SPEED = 10
@@ -24,20 +50,9 @@ func randomize_target_location():
 	update_target_location(NavigationServer3D.map_get_random_point(map, 1, true))
 
 func _process(delta: float) -> void:
-	var new_velocity = Vector3.ZERO
-	if state == States.IDLE:
-		#Beginning state. Nothing should really happen 
-		#and agent should stand still
-		randomize_target_location()
-		if !nav_agent.is_navigation_finished():
-			state = States.WALKING
-	elif state == States.WALKING:
-		var current_location = global_transform.origin
-		var next_location = nav_agent.get_next_path_position()
-		new_velocity = (next_location - current_location).normalized() * 10
-		if nav_agent.is_navigation_finished():
-			state = States.IDLE
-			new_velocity = Vector3.ZERO
+
+	var new_velocity=state.update(self)
+	# we only need one line now
 	
 	if !is_on_floor():
 		new_velocity.y -= 9.31
